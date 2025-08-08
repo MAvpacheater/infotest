7c350e2b57bf27921cd52a63698cd0f8685d9276
MAvpacheater <mavpa171@gmail.com>
2025-08-08 18:29:43 +0300

    Update aura.js

--- sites/aura.js
+++ sites/aura.js
@@ -3,12 +3,12 @@
 // Aura data
 const auraData = [
     { name: "brainrot", description: "Gives you 200% strength, 116% luck and 42% training speed", rarity: "legendary" },
-    { name: "Dark Matter Aura [1/30m]", description: "Gives you 197% strength, 116% luck and 42% training speed", rarity: "legendary" },
-    { name: "Corrupted Aura [1/20m", description: "Gives you 192% strength, 108% luck and 40% training speed", rarity: "common" },
-    { name: "Ice Shield", description: "Gives you 197% strength, 116% luck and 42% training speed", rarity: "rare" },
-    { name: "Lightning Strike", description: "Gives you 197% strength, 116% luck and 42% training speed", rarity: "epic" },
-    { name: "Healing Light", description: "Gives you 197% strength, 116% luck and 42% training speed", rarity: "common" },
-    { name: "Wind Runner", description: "Gives you 197% strength, 116% luck and 42% training speed", rarity: "rare" },
+    { name: "Dark Matter [1/30m]", description: "Gives you 197% strength, 116% luck and 42% training speed", rarity: "legendary" },
+    { name: "Corrupted [1/20m", description: "Gives you 192% strength, 108% luck and 40% training speed", rarity: "common" },
+    { name: "Galaxy [1/10m]", description: "Gives you 184% strength, 98% luck and 38% training speed", rarity: "legendary" },
+    { name: "Matrix [1/5m]", description: "Gives you 177% strength, 94% luck and 36% training speed", rarity: "epic" },
+    { name: "Universe [1/1m]", description: "Gives you 153% strength, 81% luck and 32% training speed", rarity: "epic" },
+    { name: "Storm [1/500k]", description: "Gives you 141% strength, 74% luck and 30% training speed", rarity: "rare" },
     { name: "Earth Guardian", description: "Gives you 197% strength, 116% luck and 42% training speed", rarity: "epic" },
     { name: "Mystic Vision", description: "Gives you 197% strength, 116% luck and 42% training speed", rarity: "legendary" },
     { name: "Time Warp", description: "Gives you 197% strength, 116% luck and 42% training speed", rarity: "mythic" },
